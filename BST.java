e5a902adc259aa59fab51545f558e7bff507d639
ALON1t <73937241+ALON1t@users.noreply.github.com>
2020-11-11 21:02:54 +0800

    二叉搜索树（二）

--- BST.java
+++ BST.java
@@ -57,4 +57,73 @@ public class BST {
             parent.right = node;
         }
     }
+
+    //判断
+    public boolean remove(Integer key) {
+        //查找key值所在的结点，记作node，node的双亲结点记作parent
+        Node parent = null;
+        Node cur = root;
+
+        while (cur != null) {
+            int cmp = key.compareTo(cur.key);
+            if(cmp == 0) {//找到了 进行删除操作
+                removeInternal(cur,parent);
+                return true;
+            } else if(cmp < 0){//去左子树找
+                parent = cur;
+                cur = cur.left;
+            } else { //去右子树找
+                parent = cur;
+                cur = cur.right;
+            }
+        }
+        return false;//未找到
+    }
+
+    private void removeInternal(Node node,Node parent) {
+        //情况一
+        if(node.left == null && node.right == null) {//node为叶子结点
+            if(node == root) {//node为根节点时，直接删除 root == null
+                root = null;
+            } else if(node == parent.left) {//node为parent的左孩子
+                parent.left = null;
+            } else {
+                parent.right = null;//node为parent的右孩子
+            }
+            //情况二
+        } else if(node.left != null && node.right == null) { //node只有左孩子
+            if(node == root) {
+                root = node.left;
+            } else if(node == parent.left) {
+                parent.left = node.left; //node的左孩子变为parent的左孩子
+            } else {
+                parent.right = node.left;//node的左孩子变为parent的右孩子
+            }
+            //情况三
+        } else if(node.left == null){//node只有右孩子
+            if(node == root) {
+                root = node.right;//根节点时直接让node的右孩子变为根节点
+            } else if(node == parent.left) {
+                parent.left = node.right;//node的右孩子变为parent的左孩子
+            } else {
+                parent.right = node.right;//node的右孩子变为parent的右孩子
+            }
+            //情况四
+        } else {//左右孩子都有
+            Node ghostParent = node;
+            Node ghost = node.left;
+            while(ghost.right != null) {
+                ghostParent = ghost;
+                ghost = ghost.right; //一路朝右查找 直到ghost.right == null
+            }
+            //替换
+            node.key = ghost.key;
+            //删除 ghost结点（其右孩子一定为null）
+            if(node == ghostParent) {
+                ghostParent.left = ghost.left;
+            } else {
+                ghostParent.right = ghost.left;
+            }
+        }
+    }
 }
